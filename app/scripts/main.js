b7869c9a45d1c0087000f55d3be6ab6f94593c63
jonwhite100 <jon@bpm-web-design.co.uk>
2019-05-02 22:21:48 +0100

    initial set up of futurenergies.co.uk site

---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+console.log('\'Allo \'Allo!');
+
+// Uncomment to enable Bootstrap tooltips
+// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
+// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+
+// Uncomment to enable Bootstrap popovers
+// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
+// $(function () { $('[data-toggle="popover"]').popover(); });
+
+// initialize full page plugin
+// new fullpage('#fullpage', {
+// 	//options here
+// 	autoScrolling:false,
+// 	scrollHorizontally: true
+// });
+//
+// //methods
+// fullpage_api.setAllowScrolling(false);
